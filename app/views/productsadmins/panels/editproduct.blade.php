22877672b504bf6945b187eff1955eb115c6f890
tavaresoft <joe@tavaresoft.com>
2015-04-23 19:15:19 -0400

    More Cosmetics for Sandbox
    More Pretties

--- app/views/productsadmins/panels/editproduct.blade.php
+++ app/views/productsadmins/panels/editproduct.blade.php
@@ -3,7 +3,7 @@
     <td>
       <div class="panel panel-default" style="border:1px transparent solid;border-radius:45px;color:#000000;background-color:#F0f0f0;padding:10px">
         <center>
-         <h4 data-toggle="modal" data-target="#editProduct{{$product->id}}" >{{$product->name}} General Information</h4>
+         <h4 data-toggle="modal" data-target="#editProduct{{$product->id}}" >General Information</h4>
     	   
   </div>
 </td><tr>
@@ -11,13 +11,13 @@
 
 <td>
   <div class="panel panel-default" style="border:1px transparent solid;border-radius:45px;color:#000000;background-color:#F0f0f0;padding:10px">     
-          <h4 data-toggle="modal" data-target="#editInventory{{$product->id}}" >{{$product->name}} Inventory</h4>
+          <h4 data-toggle="modal" data-target="#editInventory{{$product->id}}" >Inventory</h4>
   </div>
 </td><tr>
 
 <td data-toggle="collapse" data-parent="#accordion" href="#{{$product->id}}-collapseThree" aria-expanded="false" aria-controls="{{$product->id}}-collapseThree" >
  <div class="panel panel-default" style="border:1px transparent solid;border-radius:45px;color:#000000;background-color:#F0f0f0;padding:10px">
-      <h4 data-toggle="modal" data-target="#editImages{{$product->id}}" >{{$product->name}} Images</h4>
+      <h4 data-toggle="modal" data-target="#editImages{{$product->id}}" >Images</h4>
   </div>
 </td>
 </div>
--- app/views/productsadmins/subpanels/general.blade.php
+++ app/views/productsadmins/subpanels/general.blade.php
@@ -8,53 +8,53 @@
       </div>
       <div class="modal-body" style="background-color:#000000;color:#000000">
       	<table width="100%" style="color:#000000">
-{{Form::model($product, array('method' => 'put', 'files' => 'true', 'route' => array('updateProducts', $product->id)))}}
+			{{Form::model($product, array('method' => 'put', 'files' => 'true', 'route' => array('updateProducts', $product->id)))}}
 
-<div class="col-sm-12 col-md-6">
-	
-	
-			<label style="color:#ffffff">Product Name</label><br>
-			{{Form::text('name')}}<br>
-		
-			<label style="color:#ffffff">PayPal ID</label><br>
-			{{Form::text('paypal')}}<br>
-		
-			<label style="color:#ffffff">Product Description</label><br>
-			{{Form::textarea('description', $product->description, array('placeholder' => 'Product Description', 'style' => 'max-height:150px;max-width:200px'))}}
-			
-		
-</div>
-<div class="col-sm-12 col-md-6">
+			<div class="col-sm-12 col-md-6">
+				
+				
+						<label style="color:#ffffff">Product Name</label><br>
+						{{Form::text('name')}}<br>
+					
+						<label style="color:#ffffff">PayPal ID</label><br>
+						{{Form::text('paypal')}}<br>
+					
+						<label style="color:#ffffff">Product Description</label><br>
+						{{Form::textarea('description', $product->description, array('placeholder' => 'Product Description', 'style' => 'max-height:150px;max-width:200px'))}}
+						
+					
+			</div>
+			<div class="col-sm-12 col-md-6">
 
 
-			<label style="color:#ffffff">Price Display</label><br>
-			{{Form::text('price')}}<br>
-		
-		
-			<label style="color:#ffffff">Category</label><br>
-			<select name="category" style="color:#000000;max-width:20%">
-					<option value="{{$product->category}}">{{Str::title($product->category)}}</option>
-						@foreach(Productcat::select('name')->distinct()->get() as $cats)
-							<option value="{{$cats->name}}">{{$cats->name}}</option>
-						@endforeach
-			</select><br>
-		
+						<label style="color:#ffffff">Price Display</label><br>
+						{{Form::text('price')}}<br>
+					
+					
+						<label style="color:#ffffff">Category</label><br>
+						<select name="category" style="color:#000000;max-width:20%">
+								<option value="{{$product->category}}">{{Str::title($product->category)}}</option>
+									@foreach(Productcat::select('name')->distinct()->get() as $cats)
+										<option value="{{$cats->name}}">{{$cats->name}}</option>
+									@endforeach
+						</select><br>
+					
 
-		
-				{{Form::hidden('active', 0)}}
-				{{Form::checkbox('active')}}<label style="color:#ffffff">Active</label><br>
-			
-				{{Form::hidden('onsale', 0)}}
-				{{Form::checkbox('onsale')}}<label style="color:#ffffff">On Sale</label><br>
+					
+							{{Form::hidden('active', 0)}}
+							{{Form::checkbox('active')}}<label style="color:#ffffff">Active</label><br>
+						
+							{{Form::hidden('onsale', 0)}}
+							{{Form::checkbox('onsale')}}<label style="color:#ffffff">On Sale</label><br>
 
-				{{Form::hidden('upcomming', 0)}}
-				{{Form::checkbox('upcomming')}}<label style="color:#ffffff">Upcomming</label><br>
+							{{Form::hidden('upcomming', 0)}}
+							{{Form::checkbox('upcomming')}}<label style="color:#ffffff">Upcomming</label><br>
 
-				{{Form::hidden('preorder', 0)}}
-				{{Form::checkbox('preorder')}}<label style="color:#ffffff">Pre Order</label><br>
-		
+							{{Form::hidden('preorder', 0)}}
+							{{Form::checkbox('preorder')}}<label style="color:#ffffff">Pre Order</label><br>
+					
 
-</div>
+			</div>
 	</table>
 	</div>
     <div class="modal-footer">
